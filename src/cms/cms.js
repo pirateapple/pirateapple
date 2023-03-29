dcf19e1ae520d7e7904cf3c249ac2ad9e2b9c91a
Todd <ihatespam@gmail.com>
2023-03-29 10:51:11 -0500

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,11 @@
+import CMS from 'netlify-cms-app';
+import CategoriesControl from './categories';
+
+CMS.registerWidget('categories', CategoriesControl);
+
+// ...other registration code for collections and editor components
+
+
 // import CMS from 'netlify-cms-app'
 
 
