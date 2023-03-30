4544668aa14da7d2ff961da847d1d6a59d911378
Todd <ihatespam@gmail.com>
2023-03-30 14:54:30 -0500

    Update contact-page.js

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,13 +1,12 @@
-import { jsx } from "theme-ui";
-import React from "react";
-import { useState } from "react";
-import { graphql, navigate } from "gatsby";
-import Seo from "../components/seo";
-import Layout from "../components/siteLayout";
-import useSiteMetadata from "../hooks/SiteMetadata";
-import { Helmet } from "react-helmet";
-import Footer from "../components/footer";
-
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { graphql } from "gatsby"
+// import { RiSendPlane2Line } from "react-icons/ri"
+import Seo from "../components/seo"
+import Layout from "../components/siteLayout"
+import useSiteMetadata from "../hooks/SiteMetadata"
+import { Helmet } from "react-helmet"
+import Footer from "../components/footer"
 export const pageQuery = graphql`
   query ContactQuery($id: String!) {
     markdownRemark(id: { eq: $id }) {
@@ -16,191 +15,131 @@ export const pageQuery = graphql`
       excerpt(pruneLength: 140)
       frontmatter {
         title
-        redirect
-        contactname
-        contactphone
-        contactupload
       }
     }
     site {
       siteMetadata {
         title
       }
-    }
+    } 
   }
-`;
+`
 
 const Contact = ({ data }) => {
-  const { showNav } = useSiteMetadata();
-  const { markdownRemark, site } = data;
-  const { frontmatter, html } = markdownRemark;
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-
-
-const encode = data => {
-  console.log(data);
-  return Object.keys(data)
-    .map(key => {
-      if (key === "file") {
-        return encodeURIComponent(key) + "=" + encodeURIComponent(data[key][0].name);
-      }
-      return encodeURIComponent(key) + "=" + encodeURIComponent(data[key]);
-    })
-    .join("&");
-};
-
-const handleSubmit = e => {
-  e.preventDefault();
-  const form = e.target;
-  setIsSubmitting(true);
-  const formData = new FormData(form);
-  const data = {};
-  formData.forEach((value, key) => {
-    if (key === "file") {
-      data[key] = [value];
-    } else {
-      data[key] = value;
-    }
-  });
-  console.log(frontmatter.redirect);
-  if (frontmatter.redirect === true) {
-    setTimeout(() => {
-      window.location.href = "/thanks";
-    }, 1600);
-  } else {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("contact"),
-        ...data,
-      }),
-    })
-      .then(() => setSubmitted(true))
-      .catch(error => alert(error));
-  }
-};
-
-
-
-  const FileUploadMessage = () => (
-    <p style={{ textAlign: "right", margin: "auto", color: "#fff" }}>
-      {frontmatter.uploadtext}
-    </p>
-  );
-  
-  
-  
+  const { showNav } = useSiteMetadata()
+  const { markdownRemark, site } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
 
   return (
+
+
     <Layout className="contact-page">
       <Helmet>
-        <body className="contactpage utilitypage scroll" />
-      </Helmet>
+  <body className="contactpage utilitypage scroll" />
+</Helmet>
       <Seo
         title={frontmatter.title}
         description={frontmatter.title + " " + site.siteMetadata.title}
       />
 
-      {showNav ? <div className="spacer" style={{ height: "60px", border: "0px solid yellow" }}></div> : ""}
 
-      <div className="container panel" style={{ maxWidth: "1024px", margin: "0 auto", paddingTop: "20px" }}>
-        <h1 className="headline">{frontmatter.title}</h1>
-        <div className="description" style={{ padding: "2vh 6%" }} dangerouslySetInnerHTML={{ __html: html }} />
 
-        <div
-          className="wrapper"
-          style={{ padding: "0 10%", maxWidth: "900px", margin: "0 auto", display: "flex", flexDirection: "column", justifyContent: "center" }}
+
+
+{showNav ? (
+    <div className="spacer" style={{height:'60px', border:'0px solid yellow'}}></div>
+        ) : (
+          ""
+        )}
+      <div className="container panel" style={{ maxWidth:'1024px', margin:'0 auto',
+    paddingTop:'20px'}}>
+
+
+
+
+  
+
+
+
+     
+      <h1 className="headline">{frontmatter.title}</h1>
+
+      <div
+          className="description" style={{padding:'2vh 6%'}}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+
+        <div className="wrapper" style={{padding:'0 10%', maxWidth:'900px', margin:'0 auto', display:'flex', flexDirection:'column', justifyContent:'center' }}>
+        <form
+          className="contact-form"
+          action="/thanks"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          style={{ display:'flex', flexDirection:'column', justifyContent:'center'}}
         >
+          <input type="hidden" name="form-name" value="contact" />
+          {/* <p>
+            <label>
+              <input type="text" name="name" placeholder="Name" required />
+            </label>
+          </p> */}
+          <p>
+            <label>
+              <input type="email" name="email" placeholder="your@email.com (will not be published)" required />
+            </label>
+          </p>
+          {/* <p>
+            <label>
+              <input type="text" name="subject" placeholder="Subject" required />
+            </label>
+          </p> */}
+          <p>
+            <label>
+              <textarea name="message" placeholder="Tell us a story about your image - this will be included with your photo along with any other details you provide here" required></textarea>
+            </label>
+          </p>
 
+          <label htmlFor="attachment1" style={{padding: '0', color: 'inherit', textShadow:'1px 1px 0 #555', display:'flex', width:'100%', fontSize:'90%', gap:'15px', justifyContent:'center', alignItems:'center'}}>
+                  
+                  <input
+                      className="file-input hidden"
+                      type="file"
+                      id="attachment1"
+                      name="attachment1"
+                      // onChange={this.handleAttachment}
+                    />
+                    JPG preferred - (1600px wide)
+                    </label>
 
-<form
-  className={`contact-form ${submitted ? "submitted" : ""}`}
-  // action="/thanks"
-  name="contact"
-  method="POST"
-  data-netlify="true"
-  data-netlify-honeypot="bot-field"
-  enctype="multipart/form-data"
-  onSubmit={handleSubmit}
-  style={{
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    opacity: isSubmitting ? 0.5 : 1,
-  }}
->
-
-
-
-
-  {submitted ? (
-    <div className="thank-you-message" style={{fontSize:'200%', height:'60vh', textAlign:'center'}}>
-      Thank you - we'll be in touch!
-    </div>
-  ) : (
-    <>
-      <input type="hidden" name="form-name" value="contact" />
-
-      {frontmatter.contactname && (
-    <p>
-      <label>
-        <input type="text" name="name" placeholder="Your name" required />
-      </label>
-    </p>
-  )}
-
-      <p>
-        <label>
-          <input type="email" name="email" placeholder="your@email.com" required />
-        </label>
-      </p>
-
-      {frontmatter.contactphone && (
-    <p>
-      <label>
-        <input type="tel" name="phone" placeholder="Your phone number" />
-      </label>
-    </p>
-  )}
-
-
-      <p>
-        <label>
-          <textarea name="message" placeholder="Your Message" required></textarea>
-        </label>
-      </p>
-
-
-
-      {frontmatter.contactupload && (
-   <label htmlFor="attachment1" style={{padding: '0', color: 'inherit', textShadow:'1px 1px 0 #555', display:'flex', width:'100%', fontSize:'90%', gap:'15px', justifyContent:'center', alignItems:'center'}}>
-        <input className="file-input hidden" type="file" name="file" 
-        // accept=".pdf,.doc,.docx" 
-        />{frontmatter.uploadtext}
-      </label>
-  )}
-
-      <p
-        className="text-align-right1"
-        style={{ margin: "0 auto", color: "#fff" }}
-      >
-        <button className="button" type="submit" disabled={isSubmitting} style={{padding:'1vh 10vw'}}>
-          {isSubmitting ? "Submitting..." : "Send Now"}
-        </button>
-      </p>
-    </>
-  )}
-</form>
-
-        </div>
+          <p className="text-align-right1" style={{margin:'0 auto', color:'#fff'}}>
+            <button
+              className="button"
+              
+              type="submit"
+            >
+              Send Message{" "}
+              <span className="icon -right">
+                {/* <RiSendPlane2Line /> */}
+              </span>
+            </button>
+          </p>
+        </form>
       </div>
-      <br />
-      <br />
-      <Footer />
-    </Layout>
-  );
-};
+      </div>
 
-export default Contact;
+
+
+
+
+<br />
+<br />
+<Footer />
+</Layout>
+  )
+}
+
+export default Contact
+
+
