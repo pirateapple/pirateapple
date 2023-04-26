244ac7b43750041f6f1752dd28de9eaaf9b62693
Todd <ihatespam@gmail.com>
2023-04-26 06:02:36 -0500

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import useSiteMetadata from "../hooks/SiteMetadata"
+import useSiteMetadata from "../hooks/SiteMetadata"
 // import { StaticImage } from "gatsby-plugin-image"
 // import {Link} from "gatsby"
 import styled from "styled-components"
@@ -12,7 +12,7 @@ const CustomBox = styled.div`
 `
 
 function Test2() {
-
+  const { showNav } = useSiteMetadata()
   
   
 
@@ -22,11 +22,17 @@ function Test2() {
     <CustomBox>
 <Layout>
 <Helmet>
-        <body id="body" className="social" />
+        <body id="body" className="social scroll" />
       </Helmet>
 
-<div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'100vh', margin:'0 auto 0 auto', position:'fixed', left:'0', right:'0', top:'0'}}>
-<iframe title="VidSock" id="youtube2" className="blog-video1" width="100%" height="400" src="/admin/" frameBorder="0" playsInline  style={{position:'absolute', top:'0', left:'0', right:'0', zIndex:'0', width:'100%', height:'100vh', minHeight:'100vh', border:'0px solid yellow', borderRadius:'12px', padding:'60px 0 0 0' }} />
+      {showNav ? (
+        <div className="spacer" style={{ height: "70px", border: "0px solid yellow" }}></div>
+      ) : (
+        ""
+      )}
+
+<div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'100vh', margin:'0 auto 0 auto', position:'relative', left:'0', right:'0', top:'0'}}>
+<iframe title="VidSock" id="youtube2" className="blog-video1" width="100%" height="400" src="/admin/" frameBorder="0" playsInline  style={{position:'absolute', top:'0', left:'0', right:'0', zIndex:'0', width:'100%', height:'100vh', minHeight:'100vh', border:'0px solid yellow', borderRadius:'12px', padding:'0 0 0 0' }} />
 
 
 </div>
