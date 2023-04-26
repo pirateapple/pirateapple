3c3f5b72aaa52cbff4482e905b2ee37304d5816d
Todd <ihatespam@gmail.com>
2023-04-26 01:55:11 -0500

    fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -25,8 +25,8 @@ function Test2() {
         <body id="body" className="test" />
       </Helmet>
 
-<div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'100vh', margin:'0 auto 0 auto', position:'fixed', bottom:'0', left:'0', right:'0', top:'0'}}>
-<iframe title="VidSock" id="youtube2" className="blog-video1" width="100%" height="400" src="/admin/" frameBorder="0" playsInline  style={{position:'absolute', top:'0', left:'0', right:'0', zIndex:'0', width:'100%', height:'100vh', minHeight:'100vh', border:'0px solid yellow', borderRadius:'12px', padding:'70px 0 0 0' }} />
+<div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'100vh', margin:'0 auto 0 auto', position:'fixed', left:'0', right:'0', top:'0'}}>
+<iframe title="VidSock" id="youtube2" className="blog-video1" width="100%" height="400" src="/admin/" frameBorder="0" playsInline  style={{position:'absolute', top:'0', left:'0', right:'0', zIndex:'0', width:'100%', height:'100vh', minHeight:'100vh', border:'0px solid yellow', borderRadius:'12px', padding:'60px 0 0 0' }} />
 
 
 </div>
