597821bef431090f6d6200c7260e913c7c2585ce
Todd <ihatespam@gmail.com>
2023-04-29 05:18:41 -0500

    Update submit-form.js

--- netlify/edge-functions/submit-form.js
+++ netlify/edge-functions/submit-form.js
@@ -1,21 +1,21 @@
 const fs = require('fs-extra');
 
 exports.handler = async (event) => {
-  try {
-    const { name, email } = JSON.parse(event.body);
+  // try {
+  //   const { name, email } = JSON.parse(event.body);
 
-    const formData = `Name: ${name}\nEmail: ${email}\n`;
+  //   const formData = `Name: ${name}\nEmail: ${email}\n`;
 
-    await fs.appendFile('./public/form-data.txt', formData);
+  //   await fs.appendFile('./public/form-data.txt', formData);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Form submitted successfully' }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error submitting form' }),
-    };
-  }
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ message: 'Form submitted successfully' }),
+  //   };
+  // } catch (error) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ message: 'Error submitting form' }),
+  //   };
+  // }
 };
